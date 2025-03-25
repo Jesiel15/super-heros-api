a89bb88ad7f7e2351ba7dda1537b7a3567894f30
Jesiel Faria <fariajesiel@gmail.com>
2025-03-25 18:07:39 -0300

    Correção para AWS

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get("/super-heros", (req, res) => {
   return res.json(superHeros);
 });
 
-app.listen(port, () => {
-  console.log("Servido iniciado na porta 3000: http://localhost:3000/");
+// app.listen(port, () => {
+//   console.log("Servido iniciado na porta 3000: http://localhost:3000/");
+// });
+
+//Ajuste para AWS
+app.listen(7000, '0.0.0.0', () => {
+  console.log('Servidor iniciado na porta 7000');
 });
+
